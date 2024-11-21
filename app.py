b0508f57f87b2667157d0f5af809532f074b2093
RADHI T V J <radhitadepalli13@gmail.com>
2024-11-21 14:58:17 +0530

    a

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def process_image():
         return 'Error processing image.', 500
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0',port=5000)
--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.post('/send-email', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
