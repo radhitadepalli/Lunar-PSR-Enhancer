85c65afc79031910b3daffc8d23e812751014512
RADHI T V J <radhitadepalli13@gmail.com>
2024-11-28 01:01:14 +0530

    done

--- app.py
+++ app.py
@@ -156,4 +156,5 @@ def process_image():
         return 'Error processing image.', 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
